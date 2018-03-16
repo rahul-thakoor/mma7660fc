46fbbde09b9cdf2584589d6a3742d6fc210a6a0d
rahul-thakoor <rahul.thakoor@gmail.com>
2018-03-16 23:50:28 +0400

    constructor

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 
 
 extern crate embedded_hal as hal;
+use hal::blocking::i2c::{Write, WriteRead};
 
 pub const ADDRESS: u8 = 0x4c;
 
@@ -20,6 +21,37 @@ enum  Register {
     SR = 0x08,
     PDET = 0x09,
     PD = 0x0A,
+    STANDBY = 0x00,
+    ACTIVE =0x01
 
 
 }
+
+impl Register {
+    pub fn addr(&self) -> u8 {
+        *self as u8
+    }
+}
+
+
+/// MMA7660FC Driver
+pub struct Mma7660fc<I2C> {
+    i2c: I2C,
+}
+
+impl <I2C, E> Mma7660fc <I2C>
+where I2C : WriteRead<Error = E> + Write<Error = E>,
+
+{
+    /// Creates a new driver from a I2C peripheral
+    pub fn new(i2c: I2C) -> Result<Self, E>{
+        let mut mma7660fc = Mma7660fc { i2c };
+
+
+
+    }
+
+
+}
+
+
