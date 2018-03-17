63ccf2d748807b7e7cce2d9a84f8e8fd2a6ba730
rahul-thakoor <rahul.thakoor@gmail.com>
2018-03-17 07:06:30 +0400

    setMode and getX

--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,21 @@ enum  Register {
     MODE = 0x07,
     SR = 0x08,
     PDET = 0x09,
-    PD = 0x0A,
+    PD = 0x0A
+
+
+
+}
+
+enum Mode {
     STANDBY = 0x00,
     ACTIVE =0x01
+}
 
-
+impl Mode {
+    pub fn bits(&self) -> u8 {
+        *self as u8
+    }
 }
 
 impl Register {
@@ -47,10 +57,29 @@ where I2C : WriteRead<Error = E> + Write<Error = E>,
     pub fn new(i2c: I2C) -> Result<Self, E>{
         let mut mma7660fc = Mma7660fc { i2c };
 
+    }
+
+    /// write to register
+    pub fn write_register(&mut self,reg:Register,data:u8)->Result<(), E>{
+        self.i2c.write(ADDRESS,&[reg.addr(),data]);
+    }
+
+    /// set mode
+
+    pub fn setMode(&mut self, mode:Mode){
+        self.write_register(Register::MODE,mode);
+    }
+
+    /// get x
+    pub fn getX(&mut self) -> Result<i8,E>{
+        let mut buffer: [u8];
+
+        self.i2c.write_read(ADDRESS,&[Register::XOUT.addr()],buffer)?;
+
+        Ok((buffer) as i8);
 
 
     }
-
 
 }
 
