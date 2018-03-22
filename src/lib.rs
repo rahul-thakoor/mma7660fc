2534c7b5fffc76dccb106b930cc0d03d599d0e71
rahul-thakoor <rahul.thakoor@gmail.com>
2018-03-22 23:16:13 +0400

    get y and get z

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,43 @@
 //!
 //! [`embedded-hal`]: https://docs.rs/embedded-hal/~0.1
 //!
+//!
+//! # Example : Reading accelerometer values on RPi
+//!
+//! ```no_run
+//!     extern crate linux_embedded_hal as hal;
+//!     extern crate mma7660fc;
+//!     extern crate cast;
+//!     use std::thread;
+//!     use std::time::Duration;
+//!     use cast::*;
+//!
+//!     use hal::I2cdev;
+//!     use mma7660fc::*;
+//!
+//!     fn main(){
+//!         let dev = I2cdev::new("/dev/i2c-1").unwrap();
+//!
+//!         let mut  acc = Mma7660fc::new(dev).unwrap();
+//!
+//!         loop{
+//!             //let res = acc.get_x().unwrap();
+//!             let xyz = acc.get_xyz().unwrap();
+//!             println!("{}",xyz.x);
+//!             println!("{}",xyz.y);
+//!             println!("{}",xyz.z);
+//!
+//!             thread::sleep(Duration::from_secs(1));
+//!         }
+//!
+//!
+//!
+//!     }
+//!```
+//!
+//!
+//!
+//!
 
 #![deny(warnings)]
 #![feature(pattern_parentheses)]
@@ -123,6 +160,41 @@ where I2C : WriteRead<Error = E> + Write<Error = E>,
 		
         Ok(result)
 
+
+    }
+
+    /// get y
+    pub fn get_y(&mut self) -> Result<i8,E>{
+        let mut buffer: [u8; 1] = unsafe { mem::uninitialized() };
+
+        self.i2c.write_read(ADDRESS,&[Register::YOUT.addr()],& mut buffer)?;
+
+        // convert to 6 bits
+        let raw = u8( (buffer[0]) & 0x3F) as i8;
+        let mut result = raw;
+        if raw > 31{
+            result = raw - 64;
+        }
+
+        Ok(result)
+
+
+    }
+
+    /// get z
+    pub fn get_z(&mut self) -> Result<i8,E>{
+        let mut buffer: [u8; 1] = unsafe { mem::uninitialized() };
+
+        self.i2c.write_read(ADDRESS,&[Register::ZOUT.addr()],& mut buffer)?;
+
+        // convert to 6 bits
+        let raw = u8( (buffer[0]) & 0x3F) as i8;
+        let mut result = raw;
+        if raw > 31{
+            result = raw - 64;
+        }
+
+        Ok(result)
 
     }
 
