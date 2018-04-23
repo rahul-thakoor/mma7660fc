2a263720289863e40aae76c9b1e3ef88344f66b5
Rahul <rahul.thakoor@gmail.com>
2018-04-23 20:21:12 +0400

    previous changes

--- src/lib.rs
+++ src/lib.rs
@@ -9,34 +9,48 @@
 //! # Example : Reading accelerometer values on RPi
 //!
 //! ```no_run
-//!     extern crate linux_embedded_hal as hal;
-//!     extern crate mma7660fc;
-//!     extern crate cast;
-//!     use std::thread;
-//!     use std::time::Duration;
-//!     use cast::*;
-//!
-//!     use hal::I2cdev;
-//!     use mma7660fc::*;
-//!
-//!     fn main(){
-//!         let dev = I2cdev::new("/dev/i2c-1").unwrap();
-//!
-//!         let mut  acc = Mma7660fc::new(dev).unwrap();
-//!
-//!         loop{
-//!             //let res = acc.get_x().unwrap();
-//!             let xyz = acc.get_xyz().unwrap();
-//!             println!("{}",xyz.x);
-//!             println!("{}",xyz.y);
-//!             println!("{}",xyz.z);
-//!
-//!             thread::sleep(Duration::from_secs(1));
-//!         }
-//!
-//!
-//!
+//! extern crate linux_embedded_hal as hal;
+//! extern crate mma7660fc;
+//! extern crate cast;
+//! use std::thread;
+//! use std::time::Duration;
+//! use cast::*;
+//! 
+//! use hal::I2cdev;
+//! use mma7660fc::*;
+//! 
+//! fn main(){
+//! 
+ //!    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+//! 
+//!     
+//!     let mut  acc = Mma7660fc::new(dev).unwrap();
+//! 
+//!     loop{
+//! 
+//!         let xyzRaw = acc.get_xyz().unwrap();
+//!         let acceleration = acc.get_acceleration().unwrap();
+//!         print!("X Raw: ");
+//!         println!("{}",xyzRaw.x);
+//!         print!("Y Raw: ");
+//!         println!("{}",xyzRaw.y);
+//!         print!("Z Raw: ");
+//!         println!("{}",xyzRaw.z);
+//!         println!("");
+//! 
+//!         print!("X: ");
+//!         println!("{}",acceleration.x);
+//!         print!("Y: ");
+//!         println!("{}",acceleration.y);
+//!         print!("Z: ");
+//!         println!("{}",acceleration.z);
+//!         println!("");
+//! 
+//!         thread::sleep(Duration::from_secs(1));
 //!     }
+//! 
+//! 
+//! }
 //!```
 //!
 //!
